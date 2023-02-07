5b6bb726a4d940c382986645951937f7d6e9cc82
OihaneGomez <oihane@DESKTOP-O836G49.localdomain>
2023-02-07 13:51:11 +0100

    Init

---
+++ ckanext/auroral_theme/plugin.py
@@ -0,0 +1,14 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class AuroralThemePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic',
+            'auroral_theme')
